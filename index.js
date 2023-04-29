a9ff35d36fc57ea6ffdb4bee6c7a6e899c9c01ed
azizivahid <azizi.vahid@yahoo.com>
2023-04-29 13:31:25 -0400

    add

--- index.js
+++ index.js
@@ -1,5 +1,93 @@
 /* Your Code Here */
 
+
+function createEmployeeRecord (array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+      }
+}
+function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord);
+  }
+
+  function createTimeInEvent( dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+  
+    this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour,10),
+      date: date
+    });
+    
+    return this;
+  }  
+
+
+  function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+  
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour,10),
+      date: date
+    });
+  
+    return this;
+  }
+  
+
+  function hoursWorkedOnDate( date) {
+    const timeInEvent = this.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = this.timeOutEvents.find(event => event.date === date);
+  
+    const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+  
+    return hoursWorked;
+  }
+
+/*  function wagesEarnedOnDate( date) {
+    const hoursWorked = hoursWorkedOnDate( this,date);
+    const payRate = this.payPerHour;
+    const wagesEarned = hoursWorked * payRate;
+  
+    return wagesEarned;
+  }*/
+  let wagesEarnedOnDate = function(date){
+    let wagesEarned = hoursWorkedOnDate.call(this, date)
+        * this.payPerHour
+    return parseFloat(wagesEarned.toString())
+}
+  
+/*
+let allWagesFor = function(){
+    let eligibleDates = this.timeInEvents.map(function(e){
+        return e.date
+    })
+
+    let payable = eligibleDates.reduce(function(memo, d){
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0)
+
+    return payable
+}*/
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
+  
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
